d92993af4b97b0db30f9a327bfdf8260b98ae411
V. Bruk <vbruk@hey.com>
2021-06-18 17:51:59 -0300

    Improvements on Webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// Docs about webpacker
+// https://edgeguides.rubyonrails.org/webpacker.html
+
 
 import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
@@ -12,3 +11,6 @@ Rails.start()
 ActiveStorage.start()
 
 import "controllers"
+
+const images = require.context("../images", true)
+const imagePath = name => images(name, true)
